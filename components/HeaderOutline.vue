fa10f4b0a377e70835b9e29a6723330b2a4577f9
T.Shinohara <tshinoharawork@gmail.com>
2023-04-03 16:38:49 +0900

    toastのデバッグコマンド

--- components/HeaderOutline.vue
+++ components/HeaderOutline.vue
@@ -2,7 +2,7 @@
   <nav class="navbar navbar-light glass sticky-top">
     <div class="container-fluid max-width">
       <!-- <a class="navbar-brand" href=""></a> -->
-      <div>
+      <div @click="click()">
         {{ this.$config.env + this.$config.buildtime }}
       </div>
       <div class="">
@@ -15,7 +15,53 @@
 <script lang="ts">
 import Vue from 'vue'
 
-export default Vue.extend({})
+const message = [
+  {
+    text: 'えいっ',
+    color: 'info',
+  },
+  {
+    text: 'ファイヤー',
+    color: 'info',
+  },
+  {
+    text: 'アイスストーム',
+    color: 'info',
+  },
+  {
+    text: 'ダイアキュート',
+    color: 'success',
+  },
+  {
+    text: 'ブレインダムド',
+    color: 'success',
+  },
+  {
+    text: 'ジュゲム',
+    color: 'warning',
+  },
+  {
+    text: 'ばよえ～ん',
+    color: 'error',
+  },
+]
+
+export default Vue.extend({
+  data() {
+    return {
+      count: 0,
+    }
+  },
+  methods: {
+    click() {
+      this.$store.commit('addToast', {
+        message: message[this.count].text,
+        status: message[this.count].color,
+      })
+      if (this.count < message.length) this.count++
+    },
+  },
+})
 </script>
 <style>
 </style>

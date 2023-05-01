979cb9224b3b43082f7d58835e1ad1dc806668a8
tshinohara <takuma1201@gmail.com>
2023-05-01 19:15:23 +0900

    ﾝﾅｧ～

--- pages/setting/index.vue
+++ pages/setting/index.vue
@@ -4,9 +4,11 @@
     <a href="javascript:void(0)" @click="logout()" class="list-item"
       >ログアウト</a
     >
+    <a href="javascript:void(0)" @click="nanachi()" class="list-item"
+      >んなぁ～！</a
+    >
   </div>
 </template>
-
 
 <script lang="ts">
 import Vue from 'vue'
@@ -14,11 +16,29 @@ import { mapGetters } from 'vuex'
 
 export default Vue.extend({
   middleware: ['session'],
+  data() {
+    return {
+      process: false as boolean,
+    }
+  },
   methods: {
     logout: function () {
-      this.$atp.logout();
-      this.$store.commit("clearUser");
-      this.$router.push("/login");
+      this.$atp.logout()
+      this.$store.commit('clearUser')
+      this.$router.push('/login')
+    },
+    nanachi: function () {
+      if (!this.process) {
+        this.$atp.post({
+          text: 'ﾝﾅｧ～!!!',
+        })
+        this.process = true
+        console.log('on')
+        setTimeout(() => {
+          this.process = false
+          console.log('off')
+        }, 60000)
+      }
     },
   },
   computed: {
